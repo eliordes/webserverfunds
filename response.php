a8514ff5281f2353edca2f9538e413e3bc7707b3
Elidsza <elior.desouza2@gmail.com>
2025-09-24 12:14:24 -0400

    add form.html, response.php

---
+++ response.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Part 3.3</title>
+    <head>
+    <body>
+        <p>GET: <?= var_dump($_GET) ?></p>
+        <p>POST: <?= var_dump($_POST) ?></p>
+    </body>
+</html>
