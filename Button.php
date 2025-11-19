682eb345fed7793b8acddf83ad307a1a746fae12
Elidsza <elior.desouza2@gmail.com>
2025-11-19 12:39:41 -0500

    hi

---
+++ Button.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Toggle LED</title>
+        <?php
+            $output = `gpio toggle 0`;
+        ?>
+    <head>
+    <body>
+    </body>
+</html>
