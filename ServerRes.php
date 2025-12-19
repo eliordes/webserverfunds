f4f9d57efa7e611ae2f6f8642e6935f8d0276a9c
Elidsza <elior.desouza2@gmail.com>
2025-12-19 11:57:59 -0500

    ss

--- ServerRes.php
+++ ServerRes.php
@@ -12,13 +12,16 @@
             if (!$conn) {
               die("Connection failed: {mysqli_connect_error()}");
             }
+
+            $sql = "select * from MyTopShows;";
+            $result = mysqli_query ($conn, $sql);
         ?>
     </head>
     <body>
         <?php
             $Search_Term = htmlspecialchars($_POST['text1']); 
             $IP_Address = htmlspecialchars($_SERVER["REMOTE_ADDR"]);
-            
+
             $sql = "INSERT INTO logginginfo (IP_Address, Search_Term) 
             VALUES ($IP_Address, '$Search_Term')";
 
