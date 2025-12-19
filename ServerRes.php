889091fbccb7912f16d7a236c05444216a604855
Elidsza <elior.desouza2@gmail.com>
2025-12-19 11:24:36 -0500

    xx

---
+++ ServerRes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Response</title>
+        <?php
+            $searchstring = htmlspecialchars($_GET['text1']); 
+            $ipaddress = htmlspecialchars($_SERVER["REMOTE_ADDR"]);
+            $server = "localhost";
+            $username = "elior";
+            $password = "blueberrypi5";
+            $database = "logginginfo";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+
+            $result = mysqli_query($conn, $sql);
+        ?>
+    </head>
+    <body>
+        <?php
+            $sql = "INSERT INTO logginginfo (IP_Address, Search_Term) 
+            VALUES ($IP_Address, '$Search_Term')";
+
+            $result = mysqli_query($conn, $sql);
+
+            echo $result ? "Success!" : "Failure: {mysqli_error($conn)}";
+
+            mysqli_close($conn);
+        ?>
+    </body>
+</html>
